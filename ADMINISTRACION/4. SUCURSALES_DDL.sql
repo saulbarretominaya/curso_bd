1812cf1d300ff19b1e31cce6fcaa6165371f1af2
Saul PC Personal <saulbarretominaya@gmail.com>
2024-11-29 21:42:29 -0500

    Ordenando 3.0

---
+++ ADMINISTRACION/4. SUCURSALES_DDL.sql
@@ -0,0 +1,14 @@
+create table categorias (
+id_categoria NUMBER PRIMARY KEY,
+nombre_categoria VARCHAR2(100),
+desc_categoria VARCHAR2(500)
+-- fecha DATE DEFAULT sysdate
+);
+
+create table productos (
+id_producto NUMBER PRIMARY KEY,
+nombre_producto VARCHAR2(100),
+id_categoria NUMBER,
+CONSTRAINT fk_categoria
+FOREIGN KEY (id_categoria) REFERENCES categorias (id_categoria)
+);
