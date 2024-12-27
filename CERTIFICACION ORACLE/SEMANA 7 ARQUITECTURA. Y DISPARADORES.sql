887719252bc121ba0d981551bf69625c73c55284
Saul Barreto <saulbarretominaya@gmail.com>
2024-12-27 17:34:24 -0500

    Agregando ejercicios de trigger

--- CERTIFICACION ORACLE/SEMANA 7 ARQUITECTURA. Y DISPARADORES.sql
+++ CERTIFICACION ORACLE/SEMANA 7 ARQUITECTURA. Y DISPARADORES.sql
@@ -21,13 +21,49 @@
 -- En resumen, la arquitectura de Oracle se organiza para optimizar la memoria, la disponibilidad y el rendimiento en la gestión de bases de datos.
 
 
-CREATE OR REPLACE TRIGGER trg_clientes 
-    BEFORE INSERT ON clientes
+CREATE OR REPLACE TRIGGER trg_clientes_insert
+    BEFORE INSERT ON clientes -- BEFORE: ANTES, AFTER: DESPUES
     FOR EACH ROW 
     BEGIN
         INSERT INTO auditoria_clientes (id_cliente, nombre, ape_materno, ape_paterno, accion )
         VALUES(:NEW.id_cliente, :NEW.nombre, :NEW.ape_materno, :NEW.ape_paterno, 'INSERT');
     END
+;
+
+CREATE OR REPLACE TRIGGER trg_clientes_delete
+    BEFORE DELETE ON clientes
+    FOR EACH ROW 
+    BEGIN
+        INSERT INTO auditoria_clientes (id_cliente, nombre, ape_materno, ape_paterno, accion )
+        VALUES(:NEW.id_cliente, :NEW.nombre, :NEW.ape_materno, :NEW.ape_paterno, 'DELETE');
+    END
+;
+
+CREATE OR REPLACE TRIGGER trg_clientes_update
+    BEFORE UPDATE ON clientes
+    FOR EACH ROW 
+    BEGIN
+        INSERT INTO auditoria_clientes (id_cliente, nombre, ape_materno, ape_paterno, accion )
+        VALUES(:NEW.id_cliente, :NEW.nombre, :NEW.ape_materno, :NEW.ape_paterno, 'UPDATE');
+    END
+;
+
+--  EN ORACLE PODEMOS USAR DE ESTA MANERA SIN NECESIDAD DE CREAR VARIOS TRIGGER
+CREATE OR REPLACE TRIGGER trg_clientes
+    BEFORE INSERT OR UPDATE OR DELETE ON clientes
+    FOR EACH ROW
+BEGIN
+    IF INSERTING THEN
+        INSERT INTO auditoria_clientes (id_cliente, nombre, ape_materno, ape_paterno, accion)
+        VALUES(:NEW.id_cliente, :NEW.nombre, :NEW.ape_materno, :NEW.ape_paterno, 'INSERT');
+    ELSIF UPDATING THEN
+        INSERT INTO auditoria_clientes (id_cliente, nombre, ape_materno, ape_paterno, accion)
+        VALUES(:NEW.id_cliente, :NEW.nombre, :NEW.ape_materno, :NEW.ape_paterno, 'UPDATE');
+    ELSIF DELETING THEN
+        INSERT INTO auditoria_clientes (id_cliente, nombre, ape_materno, ape_paterno, accion)
+        VALUES(:OLD.id_cliente, :OLD.nombre, :OLD.ape_materno, :OLD.ape_paterno, 'DELETE');
+    END IF;
+END;
 ;
 
 insert into clientes (id_cliente,nombre,ape_materno,ape_paterno) values ('13','Pedro','Lopez','Muñoz');
